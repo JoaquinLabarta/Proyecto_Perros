7f79a0c1f94a231bac8ddc7f25db5f73b3c90a43
briones-gabriel <dossantosgabrielb@gmail.com>
2021-07-13 10:05:56 -0300

    Crea la conexion PDO con la BDD

---
+++ php/conexion/pdo.php
@@ -0,0 +1,20 @@
+<?php
+try {
+    // Credenciales para la conexion con la BDD.
+    $username = "root";
+    $passwd = "";
+
+    // Conexion PDO con la BDD (MySQL).
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=bromatologia",
+        $username, 
+        $passwd
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Caso de error, se muestra un mensaje destacando lo ocurrido.
+    print "[Error]: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
