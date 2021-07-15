70a8870fe5e4c1f7725b6580c65b2e4a1cfe841f
Joaquin Labarta <labartajoaquin@gmail.com>
2021-07-14 21:07:07 -0300

    problema resolvidisimo

--- php/vistas/page_login/index.php
+++ php/vistas/page_login/index.php
@@ -3,9 +3,9 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="./php/vistas/page_login/logo.png">
-        <link href="./php/vistas/page_login/index_files/bootstrap.min.css" rel="stylesheet">
-        <link href="./php/vistas/page_login/index_files/signin.css" rel="stylesheet">
+        <link rel="icon" href="logo.png">
+        <link href="/proyecto-perros/php/vistas/page_login/index_files/bootstrap.min.css" rel="stylesheet">
+        <link href="/proyecto-perros/php/vistas/page_login/index_files/signin.css" rel="stylesheet">
         <title>Bromatologia - Saladillo</title>
         <script src="./js/pageLogin.js"></script>
     </head>
@@ -17,7 +17,7 @@
                 <h3 class="h3 mb-4 font-weight-normal" style="color:#649FA5">
                     Bromatologia |  Municipalidad de Saladillo
                 </h3> 
-                <img src="./php/vistas/page_login/logo.png" alt="Logo" width="150">
+                <img src="/proyecto-perros/php/vistas/page_login/logo.png" alt="Logo" width="150">
             </div>
             <!--Inputs-->
             <div class="mb-4 input-group has-validation">
