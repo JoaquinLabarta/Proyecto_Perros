59eba50df4e208e45df9e8ec8a9b196e4d3674a7
briones-gabriel <dossantosgabrielb@gmail.com>
2021-07-16 08:26:44 -0300

    Re-ordene la pagina de logueo

--- php/vistas/page_login/index.php
+++ php/vistas/page_login/index.php
@@ -10,33 +10,37 @@
         <script src="./js/pageLogin.js"></script>
     </head>
     <body>
-        <form class="form-signin d-flex flex-column align-items-center needs-validation" method="POST" name="formularioLogin" novalidate 
-            action="./php/conexion/page_login/action.php">
+        <div class="d-flex flex-column align-items-center border-primary">
             <!--Header-->
             <div class="text-center mb-4">
                 <h3 class="h3 mb-4 font-weight-normal" style="color:#649FA5">
-                    Bromatologia |  Municipalidad de Saladillo
+                Bromatologia |  Municipalidad de Saladillo
                 </h3> 
                 <img src="/proyecto-perros/php/vistas/page_login/logo.png" alt="Logo" width="150">
             </div>
-            <!--Inputs-->
-            <div class="mb-4 input-group has-validation">
-                <input type="text" name="inputUser" class="form-control" placeholder="Ingrese su usuario" required>
-                <div class="invalid-feedback">
+            <form class="form-signin needs-validation" method="POST" 
+                name="formularioLogin" novalidate action="./php/conexion/page_login/action.php">
+                <!--Inputs-->
+                <div class="mb-4 input-group has-validation">
+                    <input type="text" name="inputUser" class="form-control" placeholder="Ingrese su usuario" required>
+                    <div class="invalid-feedback">
                     Por favor ingrese su usuario.
+                    </div>
                 </div>
-            </div>
-            <div class="mb-4 input-group has-validation">
-                <input type="password" name="inputPassword" class="form-control" placeholder="Ingrese su clave" required>
-                <div class="invalid-feedback">
+                <div class="mb-4 input-group has-validation">
+                    <input type="password" name="inputPassword" class="form-control" placeholder="Ingrese su clave" required>
+                    <div class="invalid-feedback">
                     Por favor ingrese su clave.
+                    </div>
                 </div>
-            </div>
-            <!--Botones de submit-->
-            <button class="btn btn-lg btn-danger mb-4" type="submit" style="color: D0757C;">
-                Ingresar
-            </button>
-            <a href="#">Ingresar como invitado</a>
-        </form>
+                <!--Botones de submit-->
+                <div class="d-flex align-items-center flex-column">
+                    <button class="btn btn-lg btn-danger mb-4" type="submit" style="color: D0757C;">
+                    Ingresar
+                    </button>
+                    <a href="#">Ingresar como invitado</a>
+                </div>
+            </form>
+        </div>
     </body>
 </html>
