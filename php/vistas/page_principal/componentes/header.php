630ba4fb976dcd35469c6c3f682e8bb31b7699bb
briones-gabriel <dossantosgabrielb@gmail.com>
2021-08-03 10:20:16 -0300

    Organice la pagin principal

---
+++ php/vistas/page_principal/componentes/header.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" style="color:#649FA5">
+      <img src="/proyecto-perros/recursos/logo.png" alt="" width="40" height="40">
+      Bromatologia | Municipalidad de Saladillo
+      <div>
+        <button class="btn btn-white" style="color:#649FA5"> <img src="/proyecto-perros/recursos/home.png" alt="" width="24" height="24"> <a href="indexadmin.php">Inicio</a></button>
+        <button class="btn btn-white" style="color:#649FA5"> <img src="/proyecto-perros/recursos/user.png" alt="" width="24" height="24"> <a href="paginausuarios.php">Usuarios</a></button>
+        <button class="btn btn-white" style="color:#649FA5"> <img src="/proyecto-perros/recursos/register.png" alt="" width="24" height="24"> <a href="indexinvitados.php">Agregar</a></button>
+      </div>
+    </a>
+  </div>
+</nav>
--- php/vistas/page_principal/indexusuarios.php
+++ php/vistas/page_principal/indexusuarios.php
@@ -1,80 +1,79 @@
 <?php
 include_once '../../conexion/pdo.php';
 
-$sql_leer='SELECT * FROM Usuarios';
-$gsent=$pdo->prepare($sql_leer);
+$sql_leer = 'SELECT * FROM Usuarios';
+$gsent = $pdo->prepare($sql_leer);
 $gsent->execute();
-$usuarios=$gsent->fetchAll();
+$usuarios = $gsent->fetchAll();
 ?>
 <!doctype html>
 <html lang="es">
-  <head>
-    <meta name="viewport" content="width=device-width" />
-    <meta charset="utf-8">
-    <link rel="icon" href="icons/logo.png">
-    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-    <link rel="stylesheet" href="Datatables-1.10.24/css/jquery.dataTables.min.css">
-    <link href="estilos.css" rel="stylesheet" type="text/css">
-    <title>Datatable PHP</title>
-      
-  </head>
-  <body>
-  <div class="shadow-sm p-0 mb-5 bg-body rounded">
-  <nav class="navbar navbar-light">
-  <div class="container-fluid">
-    <a class="navbar-brand"  style="color:#649FA5">
-      <img src="icons/logo.png" alt="" width="40" height="40">
-      Bromatologia | Municipalidad de Saladillo  
-            <div class="btn-group">
-            <button type="button" class="btn btn-white" style="color:#649FA5"  > <img src="icons/house.png" alt="" width="24" height="24"> Inicio</button>
-            <button type="button" class="btn btn-white" style="color:#649FA5"> <img src="icons/register.png" alt="" width="24" height="24">Agregar</button>  
-          </div>
-    </a>
-  </div>
-</div>
-</nav>
+
+<head>
+  <meta name="viewport" content="width=device-width" />
+  <meta charset="utf-8">
+
+  <link rel="icon" href="/proyecto-perros/recursos/logo.png">
+  <link href="/proyecto-perros/css/page_principal/styles.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+
+  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
+
+  <title>Datatable PHP</title>
+</head>
+
+<body>
+  <?php include_once "./componentes/header.php"; ?>
 
   <br>
-    <div class="container">
-       <div class="row">
-           <div class="col-lg-12">
-            <table id="usuarios" class="table table-striped">
-                <thead>
-                    <th class = "text-center">ID</th>
-                    <th class = "text-center">Usuario</th>
-                    <th class = "text-center">Nombre</th>
-                    <th class = "text-center">Apellido</th>
-                    <th class = "text-center">Nacimiento</th>
-                    <th class = "text-center">Clave</th>
-                    <th class = "text-center">Administrador</th>
-                    <th class = "text-center">Activo</th>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach($usuarios as $usuario){
-                    ?>
-                    <tr>
-                        <td><?php echo $usuario['UsuarioID']?></td>
-                        <td><?php echo $usuario['Usuario']?></td>
-                        <td><?php echo $usuario['Nombre']?></td>
-                        <td><?php echo $usuario['Apellido']?></td>
-                        <td><?php echo $usuario['Nacimiento']?></td>
-                        <td><?php echo $usuario['Clave']?></td>
-                        <td><?php echo $usuario['Administrador']?></td>
-                        <td><?php echo $usuario['Activo']?></td>
-                    </tr>
-                    <?php
-                        }
-                    ?>
-                </tbody>
-            </table>
-           </div>
-       </div> 
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <table id="usuarios" class="table table-striped">
+          <thead>
+            <th class="text-center">ID</th>
+            <th class="text-center">Usuario</th>
+            <th class="text-center">Nombre</th>
+            <th class="text-center">Apellido</th>
+            <th class="text-center">Nacimiento</th>
+            <th class="text-center">Clave</th>
+            <th class="text-center">Administrador</th>
+            <th class="text-center">Activo</th>
+          </thead>
+          <tbody>
+            <?php
+            foreach ($usuarios as $usuario) {
+            ?>
+              <tr>
+                <td><?php echo $usuario['UsuarioID'] ?></td>
+                <td><?php echo $usuario['Usuario'] ?></td>
+                <td><?php echo $usuario['Nombre'] ?></td>
+                <td><?php echo $usuario['Apellido'] ?></td>
+                <td><?php echo $usuario['Nacimiento'] ?></td>
+                <td><?php echo $usuario['Clave'] ?></td>
+                <td><?php echo $usuario['Administrador'] ?></td>
+                <td><?php echo $usuario['Activo'] ?></td>
+              </tr>
+            <?php
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <!--JavaScript -->
-    <script src="jQuery-3.3.1/jquery-3.3.1.js"></script>
-    <script src="DataTables-1.10.24/js/jquery.dataTables.min.js"></script> 
-    <script src="js/script.js"></script>
+  </div>
+</body>
 
-  </body>
+<!--LINK: https://cdn.datatables.net/-->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="/proyecto-perros/js/page_principal/scripts.js"></script>
+<script src="/proyecto-perros/js/modulos/pdfmake/pdfmake.js" type="module"></script>
+<script src="/proyecto-perros/js/modulos/jszip/jszip.js" type="module"></script>
+
+<?php include_once "./componentes/modals.php"; ?>
+
 </html>
