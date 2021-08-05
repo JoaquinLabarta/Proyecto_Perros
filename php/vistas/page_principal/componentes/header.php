b7c8bb0255bb252355ef95096efff9a367fc4c43
briones-gabriel <dossantosgabrielb@gmail.com>
2021-08-05 19:25:47 -0300

    Arreglo del header

--- php/vistas/page_principal/componentes/header.php
+++ php/vistas/page_principal/componentes/header.php
@@ -3,11 +3,20 @@
     <a class="navbar-brand" style="color:#649FA5">
       <img src="/proyecto-perros/recursos/logo.png" alt="" width="40" height="40">
       Bromatologia | Municipalidad de Saladillo
-      <div>
-        <button class="btn btn-white" style="color:#649FA5"> <img src="/proyecto-perros/recursos/home.png" alt="" width="24" height="24"> <a href="indexadmin.php">Inicio</a></button>
-        <button class="btn btn-white" style="color:#649FA5"> <img src="/proyecto-perros/recursos/user.png" alt="" width="24" height="24"> <a href="paginausuarios.php">Usuarios</a></button>
-        <button class="btn btn-white" style="color:#649FA5"> <img src="/proyecto-perros/recursos/register.png" alt="" width="24" height="24"> <a href="indexinvitados.php">Agregar</a></button>
-      </div>
     </a>
+    <div>
+      <button class="btn btn-white" style="color:#649FA5">
+        <img src="/proyecto-perros/recursos/home.png" alt="" width="24" height="24">
+        <a href="indexadmin.php">Inicio</a>
+      </button>
+      <button class="btn btn-white" style="color:#649FA5">
+        <img src="/proyecto-perros/recursos/user.png" alt="" width="24" height="24">
+        <a href="paginausuarios.php">Usuarios</a>
+      </button>
+      <button class="btn btn-white" style="color:#649FA5">
+        <img src="/proyecto-perros/recursos/register.png" alt="" width="24" height="24">
+        <a href="indexinvitados.php">Agregar</a>
+      </button>
+    </div>
   </div>
 </nav>
