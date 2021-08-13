bd1b1da02c904f9e17b054f43a02ec50f9f3ed46
briones-gabriel <dossantosgabrielb@gmail.com>
2021-08-13 08:48:28 -0300

    Cambie la forma en la que se ingresa a la pagina pricipal desde la pagina de logueo

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-include_once "./php/vistas/page_login/index.php";
+session_start();
+
+if ($_SESSION) {
+  header("Location: /proyecto-perros/php/vistas/page_principal");
+}
+
+include_once "/proyecto-perros/php/vistas/page_login/index.php";
--- php/conexion/get_users.php
+++ php/conexion/get_users.php
@@ -1,7 +1,9 @@
 <?php
 include_once "pdo.php";
 
-$query = "SELECT UsuarioID, Usuario, Nombre, Apellido, Nacimiento, Email, Clave, Administrador, Activo FROM Usuarios";
+$query = "SELECT UsuarioID, Usuario, Nombre, Apellido, Nacimiento, Email, Clave, Administrador, Activo 
+    FROM Usuarios";
+
 $sql = $pdo->prepare($query);
 $sql->execute();
 $usuarios = $sql->fetchAll();
