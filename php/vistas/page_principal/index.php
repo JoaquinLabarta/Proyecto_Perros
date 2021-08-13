02a9b47728f2abb892b7bb2b1294e471be537d18
Joaquin Labarta <labartajoaquin@gmail.com>
2021-08-13 10:28:55 -0300

    pagina usuarios front-end hecho

--- php/vistas/page_principal/index.php
+++ php/vistas/page_principal/index.php
@@ -30,35 +30,40 @@ $carpeta_actual = basename(getcwd());
 
   <br>
   <div class="container">
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Buscar Perro por nombre, raza, etc..." aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+      <br>
     <?php if (!$_SESSION["Invitado"]) : ?>
       <button class="btn btn-primary px-3" data-bs-toggle="modal" data-bs-target="#agregarPerro">
         Agregar perro
       </button>
     <?php endif; ?>
     <div class="row">
-      <div class="col-lg-12 table-responsive">
-        <table id="usuarios" class="table table-striped">
+      <div class="col-lg-12 table-responsive"><br>
+        <table id="usuarios" class="table table-hover table-bordered">
           <thead>
-            <th>ID</th>
-            <th>Usuario</th>
-            <th>Nombre</th>
-            <th>Administrador</th>
-            <th>Activo</th>
-            <th>Acciones</th>
+            <th class = "text-center">ID</th>
+            <th class = "text-center">Usuario</th>
+            <th class = "text-center">Nombre</th>
+            <th class = "text-center">Administrador</th>
+            <th class = "text-center">Activo</th>
+            <th class = "text-center">Acciones</th>
           </thead>
           <tbody>
             <?php
             include "../../conexion/get_users.php";
             foreach ($usuarios as $usuario) : ?>
               <tr>
-                <td><?php echo $usuario['UsuarioID'] ?></td>
-                <td><?php echo $usuario['Usuario'] ?></td>
-                <td><?php echo $usuario['Nombre'] . " " . $usuario['Apellido'] ?></td>
-                <td><?php echo $usuario['Administrador'] ? "Si" : "No"; ?></td>
-                <td><?php echo $usuario['Activo'] ? "Si" : "No"; ?></td>
-                <td>
-                  <button>Editar</button>
-                  <button>Eliminar</button>
+                <td class = "text-center"><?php echo $usuario['UsuarioID'] ?></td>
+                <td class = "text-center"><?php echo $usuario['Usuario'] ?></td>
+                <td class = "text-center"><?php echo $usuario['Nombre'] . " " . $usuario['Apellido'] ?></td>
+                <td class = "text-center"><?php echo $usuario['Administrador'] ? "Si" : "No"; ?></td>
+                <td class = "text-center"><?php echo $usuario['Activo'] ? "Si" : "No"; ?></td>
+                <td class = "text-center">
+                  <i class="fas fa-user-edit"></i>
+                  <i class="fas fa-user-times"></i>
                 </td>
               </tr>
             <?php endforeach; ?>
@@ -70,13 +75,13 @@ $carpeta_actual = basename(getcwd());
 </body>
 
 <!--LINK: https://cdn.datatables.net/-->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/de1cdf12c2.js" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="/proyecto-perros/js/page_principal/scripts.js"></script>
 <script src="/proyecto-perros/js/modulos/pdfmake/pdfmake.js" type="module"></script>
 <script src="/proyecto-perros/js/modulos/jszip/jszip.js" type="module"></script>
 
-<?php include_once "./componentes/modals.php"; ?>
+<?php include_once "../componentes/modals.php"; ?>
 
 </html>
