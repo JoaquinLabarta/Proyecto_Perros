c9e644c395c6afb1f19e7e86a7f78c4a651104ff
Joaquin Labarta <labartajoaquin@gmail.com>
2021-09-14 07:47:16 -0300

    Barra de Navegacion Terminada

--- php/vistas/componentes/header.php
+++ php/vistas/componentes/header.php
@@ -13,15 +13,8 @@
         border-radius: .25rem;
         transition: box-shadow .15s ease-in-out;
     }
-
-    .btn-group-vertical {
-        flex-direction: column;
-        align-items: center;
-    }
-
-    .btn-group-vertical>.btn,
-    .btn-group-vertical>.btn-group {
-        width: 60%;
+    .btn{
+        margin-left: 5px;
     }
 </style>
 
@@ -34,6 +27,7 @@
         <div class="d-flex">
             <?php if ($_SESSION["Rol"] == 1): ?>
                 <div class="dropdown me-2">
+                    <div class="d-flex">
                     <button class="btn btn-white border" style='color: #649fa5' type="button" data-bs-toggle="dropdown">
                         <i class="fas fa-plus"></i>
                     </button>
@@ -61,51 +55,28 @@
                     <?php endif; ?>
                 </div>
             <?php endif; ?>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                <img src="/proyecto-perros/recursos/nav.png" class="navbar-toggler-icon"></img>
-            </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <a class="navbar-brand" style="color:#649FA5" href="/proyecto-perros/php/vistas/page_principal">
-                        <img src="/proyecto-perros/recursos/logo.png" alt="" width="40" height="40">
-                        Bromatologia Saladillo
-                    </a>
-                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
-                <div class="btn-group-vertical">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <?php if ($_SESSION["Rol"] == 1): ?>
-                            <li class="nav-item">
                                 <?php if ($carpeta_actual !== "page_principal"): ?>
-                                    <a class='btn btn-white' title="Usuarios" style='color: #649fa5' href="/proyecto-perros/php/vistas/page_principal">
-                                        <i class="fas fa-home"></i> Inicio
+                                    <a class='btn btn-white border' title="Usuarios" style='color: #649fa5' href="/proyecto-perros/php/vistas/page_principal">
+                                        <i class="fas fa-home"></i> 
                                     </a>
                                 <?php endif; ?>
-                            </li>
-                            <li class="nav-item">
                                 <?php if ($carpeta_actual !== "page_usuarios"): ?>
-                                    <a class="btn btn-white" title="Usuarios" style='color: #649fa5' href="/proyecto-perros/php/vistas/page_usuarios">
-                                        <i class="fas fa-users"></i> Usuarios
+                                    <a class="btn btn-white border" title="Usuarios" style='color: #649fa5' href="/proyecto-perros/php/vistas/page_usuarios">
+                                        <i class="fas fa-users"></i>
                                     </a>
                                 <?php endif; ?>
-                            </li>
-
-                            <li class="nav-item">
                                 <?php if ($carpeta_actual !== "page_propietarios"): ?>
-                                    <a class='btn btn-white' title="Propietarios" style='color: #649fa5' href="/proyecto-perros/php/vistas/page_propietarios">
-                                        <i class="fas fa-id-card"></i> Propietarios
+                                    <a class='btn btn-white border' title="Propietarios" style='color: #649fa5' href="/proyecto-perros/php/vistas/page_propietarios">
+                                        <i class="fas fa-id-card"></i>
                                     </a>
                                 <?php endif; ?>
-                            </li>
                         <?php endif; ?>
-                        <li class="nav-item">
-                            <a class="btn btn-white" title="Cerrar sesion" style="color: #649fa5" href="/proyecto-perros/php/conexion/page_principal/cerrar_sesion.php">
-                                <i class="fas fa-sign-out-alt"></i> Cerrar Sesion
+                            <a class="btn btn-white border" title="Cerrar sesion" style="color: #649fa5" href="/proyecto-perros/php/conexion/page_principal/cerrar_sesion.php">
+                                <i class="fas fa-sign-out-alt"></i>
                             </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+                        </div>
         </div>
     </div>
 </nav>
