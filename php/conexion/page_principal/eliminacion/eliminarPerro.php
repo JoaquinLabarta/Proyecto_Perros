f5b64400b0c2e17e3314f28b0b61f3fb56ddf16f
briones-gabriel <dossantosgabrielb@gmail.com>
2021-09-29 19:01:06 -0300

    Arregle el problema para eliminar propietarios

--- php/conexion/page_principal/eliminacion/eliminarPerro.php
+++ php/conexion/page_principal/eliminacion/eliminarPerro.php
@@ -6,16 +6,11 @@ $perroId = $_POST["perroId"];
 
 // Queries para borrar al propietario del perro de PropietariosPerros
 // Y para borrar al perro de Perros
-$deletePropietarioRelation =
-    "DELETE FROM PropietariosPerros WHERE PerroId = :perroId";
 $deletePerro = "DELETE FROM Perros WHERE PerroId = :perroId";
 
 $params = ["perroId" => $perroId];
 
 try {
-    $resultPropietarioRelation = $pdo
-        ->prepare($deletePropietarioRelation)
-        ->execute($params);
     $resultPerro = $pdo->prepare($deletePerro)->execute($params);
 } catch (\Throwable $th) {
     echo "Hubo un error al intentar eliminar un perro.";
